463beb95225b6656c608752667b7169110fd4ac1
Prasheel12 <prasheel.bhagalia@uoit.net>
2018-03-05 23:05:32 -0500

    prasheel - commented

--- question5.c
+++ question5.c
@@ -7,11 +7,15 @@
 int total_sum, status;
 sem_t sem;
 
-void* factorial_calc(int number){
-    int factorial;
-    for(int i=1;i<number;i++){
+//Calculates the factorial of the number entered as arguement
+void* factorial_calc(int numb){
+    int factorial = 1;
+
+    for(int i=1;i<=numb;i++){
         factorial *= i;
     }
+
+    printf("Factorial: %d\n", factorial);
     sem_wait(&sem);
     total_sum += factorial;
     sem_post(&sem);
@@ -20,56 +24,69 @@ void* factorial_calc(int number){
 int main(void)
 {
     FILE *fp;
+    //Amount of numbers asked to input
     int NUMBERS = 5;
     
-    int input[NUMBERS];
+    //Array for the numbers entered
+    int enteredNum[NUMBERS];
+
+
     printf("Please enter 5 numbers to write to file.\n");
     
     for(int i=0; i<NUMBERS; i++){
-        scanf("%d", &input[i]);
+        scanf("%d", &enteredNum[i]);
     }
+
 
     fp = fopen("numbers.txt", "w");
     for(int b=0; b<NUMBERS; b++){
-        fprintf(fp, "%d", input[NUMBERS]);
+        fprintf(fp, "%d", enteredNum[NUMBERS]);
     }
     fclose(fp);
 
     pid_t pid1 = 0;
 
+    //Creates a child process
     pid1 = fork();
 
+    //pid1 does not return 1 or 0 meaning it is not the child or parent
     if(pid1==-1){
         printf("Error creating process");
         return 0;
     }
 
+    //Initalizes semaphore
     sem_init(&sem, 0, 1);
 
+    //Child process
     if(pid1==0){
 
+        //Array for all the numbers entered
         int numEntered[NUMBERS];
 
+        //File pointer created to read the file and store inside numEntered array
         FILE *fp = fopen("numbers.txt", "r");
-
         for(int a = 0; a<NUMBERS; a++){
             fscanf(fp,"%d\n", &numEntered[a]);
         }
-        
         fclose(fp);
 
+        //Creates an array of threads
         pthread_t tid[NUMBERS];
         pthread_attr_t attr;
         pthread_attr_init(&attr);
 
+        //Creates 5 threads
         for(int i=0; i<NUMBERS; i++){
             pthread_create(&tid[i],NULL,factorial_calc,numEntered[i]);
         }
 
+        //Joins all the threads
         for(int j=0; j<NUMBERS; j++){
             pthread_join(tid[j],0);
         }
 
+        //File pointer created to create/overwrite sum.txt with total
         FILE *fp2; 
         fp2 = fopen("sum.txt","w");
         fprintf(fp2, "%d", total_sum);
@@ -79,9 +96,12 @@ int main(void)
     }
 
     else{
+        //Waits for child process to terminate
         waitpid(pid1,&status,0);
         int printSum;
+        //Child process is terminated
         if(status==0){
+            //File pointer created to read from the file and print the total sum
             FILE *fp;
             fp = fopen("sum.txt","r");
             fscanf(fp, "%d", &printSum);

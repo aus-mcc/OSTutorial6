f69750a9e9452d1f539138b1b1bb7913a9647d23
Prasheel12 <prasheel.bhagalia@uoit.net>
2018-03-05 19:38:43 -0500

    prasheel commit

--- question1.c
+++ question1.c
@@ -1,6 +1,72 @@
+#define _XOPEN_SOURCE 700 // required for barriers to work
+#include <stdlib.h>
 #include <stdio.h>
-
-int main(){
-
+#include <pthread.h>
+#include <semaphore.h>
+#include <errno.h>
+int main(void)
+{
     
+    FILE *file1;
+    FILE *file2;
+
+    pid_t pid1 = 0;
+    pid1 = fork();
+
+    //Not parent or child
+    if(pid1 == -1){
+        fprintf(stderr, "can't fork, error %d\n",errno);
+        exit(EXIT_FAILURE);
+    }
+ 
+    //Executes if it is a child
+    if(pid1 == 0){
+        char read[256];
+        sleep(1);
+        file1 = fopen("child1.txt", "r");
+        fscanf(file1, "%s", read);
+        printf("%s",read);
+        return 0;
+    }
+
+    //Executes if parent
+    else{
+
+        pid_t pid2 = fork();
+        
+        //Neither parent or child
+        if(pid2 == -1){
+               fprintf(stderr, "can't fork, error %d\n",errno);
+                exit(EXIT_FAILURE);
+        }
+
+         //Creates the second child
+         if(pid2 == 0){
+            char read2[256];
+            sleep(1);
+            file2 = fopen("child2.txt", "r");
+            fscanf(file2, "%s", read2);
+            printf("%s",read2);
+           return 0;
+        }
+
+        //Parent - creates 2 text files and writes to it 
+        else{
+
+            file1 = fopen("child1.txt","w");
+            file2 = fopen("child2.txt","w");
+
+            if(file1 == NULL|| file2 == NULL){
+                printf("Error opening file\n");
+                exit(1);
+             }
+
+            const char *text = "Child1";
+            const char *text2 = "Child2";
+
+            fprintf(file1, "%s", text);
+            fprintf(file2, "%s", text2);
+        }
+    }
+    return 0;
 }

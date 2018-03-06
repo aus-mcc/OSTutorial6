3e8d677302976172ef724c919857ad3fc840d074
Prasheel12 <prasheel.bhagalia@uoit.net>
2018-03-05 19:40:57 -0500

    original austin commit

---
+++ question2.c
@@ -0,0 +1,29 @@
+#define _XOPEN_SOURCE 700 // required for barriers to work
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <semaphore.h>
+int main(void)
+{
+    int pid;
+
+    pid = fork();
+
+    if(pid == -1){
+        printf("Unable to fork");
+        exit(EXIT_FAILURE);
+    }
+    else if(pid == 0){
+        // Child Process
+        sleep(1);
+        printf("Child Process\n");
+        exit(1);
+    }
+    else{
+        int status;
+        if(wait(&status)){
+            printf("Parent Process\n");
+        }
+    }
+    return 0;
+}

f13bfbcc7362351769ee02240d6b563a4b0b4626
Prasheel12 <prasheel.bhagalia@uoit.net>
2018-03-05 23:23:31 -0500

    prasheel commit - commented

--- question1.c
+++ question1.c
@@ -6,11 +6,13 @@
 #include <errno.h>
 int main(void)
 {
-    
+    //File pointers for each file (child1.txt and child2.txt)
     FILE *file1;
     FILE *file2;
 
+    
     pid_t pid1 = 0;
+    //Create child process
     pid1 = fork();
 
     //Not parent or child
@@ -19,7 +21,7 @@ int main(void)
         exit(EXIT_FAILURE);
     }
  
-    //Executes if it is a child
+    //Child process
     if(pid1 == 0){
         char read[256];
         sleep(1);
@@ -29,9 +31,10 @@ int main(void)
         return 0;
     }
 
-    //Executes if parent
+    //Parent process
     else{
 
+        //Creates second child process
         pid_t pid2 = fork();
         
         //Neither parent or child
@@ -40,7 +43,7 @@ int main(void)
                 exit(EXIT_FAILURE);
         }
 
-         //Creates the second child
+         //Second child process
          if(pid2 == 0){
             char read2[256];
             sleep(1);
@@ -53,17 +56,21 @@ int main(void)
         //Parent - creates 2 text files and writes to it 
         else{
 
+            //Opens each text file
             file1 = fopen("child1.txt","w");
             file2 = fopen("child2.txt","w");
 
+            //If either are NULL, print error
             if(file1 == NULL|| file2 == NULL){
                 printf("Error opening file\n");
                 exit(1);
              }
 
+            //Required words to write to file
             const char *text = "Child1";
             const char *text2 = "Child2";
 
+            //Writes words above to respective file
             fprintf(file1, "%s", text);
             fprintf(file2, "%s", text2);
         }
